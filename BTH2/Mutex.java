7933d2356b2c968382c77206ad4a82dece1b212a
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2022-06-28 22:50:21 +0700

    Add code thread and mutex

---
+++ BTH2/Mutex.java
@@ -0,0 +1,50 @@
+public class Mutex {
+    /* Chương trình khởi tạo 2 luồng cùng truy cập trực tiếp và thay đổi giá trị của đối tượng Counter */
+    public static void main(String[] args) {
+        Counter c = new Counter();
+        try {
+            // create thread 1
+            Thread thread1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 20000; i++) {
+                        c.increment();
+                    }
+                }
+            });
+
+            // create thread 2
+            Thread thread2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 20000; i++) {
+                        c.increment();
+                    }
+                }
+            });
+
+            thread1.start();
+            thread2.start();
+            thread1.join();
+            thread2.join();
+            
+            /* In ra kết quả cuối cùng của biến bên trong đối tượng */
+            System.out.println("Counter = " + c.getCount());
+        }
+        catch (Exception e) {e.printStackTrace();}
+
+        // System.out.println("Hello");
+    }
+}
+
+
+class Counter {
+    private int count;
+    /* Từ khóa synchronized đóng vai trò như khóa mutex để đồng bộ dữ liệu giữa 2 luồng */
+    public synchronized void increment() {
+        count++;
+    }
+    public int getCount() {
+        return count;
+    }
+}
---
+++ BTH2/mutex.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int counter;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_t tid[2];
+void* threadFunc(void* param) {
+    pthread_mutex_lock(&lock); /* Khóa tài nguyên để tránh xung đột */
+    for (int i = 0; i < 20000; i++) {
+        counter++;
+    }
+    pthread_mutex_unlock(&lock); /* Mở khóa tài nguyên để các luồng khác sử dụng */
+    pthread_exit(NULL);
+}
+
+int main() {
+    counter = 0;
+    int ret = 0;
+    for (int i = 0;i< 2;i++) {
+        ret = pthread_create(&(tid[i]), NULL, threadFunc, NULL);
+        if (ret != 0) {
+            printf("Thread [%d] created error\n", i);
+        }
+    }
+
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    printf("Counter = %d\n", counter);
+    return 0;
+}
